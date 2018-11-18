a49a394e4709b1bba75f5cd0e90ecdc5d32040b2
theoldmoon0602 <theoldmoon0602@gmail.com>
2018-11-18 09:45:22 +0900

    trivial change

--- def_re.py
+++ def_re.py
@@ -2,8 +2,8 @@ import subprocess
 
 
 def tokens():
-    container_id = 'f88f372f7f1c'
-    o = subprocess.check_output(['docker', 'exec', container_id, 'sh', '/getandclean.sh'])
+    container_id = subprocess.check_output("docker ps | grep re | awk '{print $1}'", shell=True).strip()
+    o = subprocess.check_output(['docker', 'exec', container_id, 'sh', '/getandclean.sh']).strip()
     return set(o.split("\n"))
 
 print(tokens())
